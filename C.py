f5ccb9c5cc8a0bc56225b108a4116a0880a3bdcf
vactasyy <167777001+vactasyy@users.noreply.github.com>
2025-05-19 18:22:05 +0700

    C.py

--- C.py
+++ C.py
@@ -1,17 +1,16 @@
 def hitung_huruf(s):
     from collections import defaultdict
 
-    s = s.replace(" ", "")  # Hilangkan spasi
+    s = s.replace(" ", "")
     huruf_dict = defaultdict(int)
 
     for char in s:
-        if char.isalpha():  # Hanya huruf
+        if char.isalpha():
             huruf_dict[char] += 1
 
     hasil = dict(sorted(huruf_dict.items()))
     print("C:", hasil)
 
-# Contoh penggunaan:
 hitung_huruf("Hello World")
 hitung_huruf("Bismillah")
 hitung_huruf("MasyaAllah")
